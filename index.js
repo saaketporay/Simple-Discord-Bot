c7f210a821076fac30ecf6a8343fe178f35cadc2
Saaket Poray <saaketporay@gmail.com>
2019-11-06 19:58:56 -0600

    bug fixes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const {DISCORD_BOT_TOKEN} = process.env;
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var counter = 0;
+
 client.on('ready', () => {
     console.log('Logged in as ${client.user.tag}!');
 });
@@ -20,6 +22,15 @@ client.on('message', msg => {
     if(msg.content.toLowerCase() === 'haha'){
         msg.reply('hahahaha')
     }
+    if(msg.content === 'number'){
+        msg.reply(counter)
+    }
+    if(msg.content === 'add'){
+        counter++;
+        msg.reply(counter)
+    }
+
+
 });
 
 client.login(DISCORD_BOT_TOKEN);
